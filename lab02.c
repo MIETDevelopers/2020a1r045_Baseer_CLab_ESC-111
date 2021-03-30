513d0a2e688e1152434f47e17286c589e8413fcd
unknown <baseerfatima01@gmail.com>
2021-03-30 23:18:46 +0530

    addition , subtraction, multiplication,division of two numbers

--- lab02.c
+++ lab02.c
@@ -1,24 +1,20 @@
 #include<stdio.h>
-
 int main() {
-
-    int a, b, sum, sub, div, mul;
-    printf("enter two number\n");
-    scanf("%d%d",&a,&b);    
+    int a,b,sum,sub,div,mul;
+    printf("enter two numbers\n");
+    scanf("%d%d", &a,&b);
     // addition of two numbers
     sum=a+b;
     printf("addition of two numbers is %d\n",sum);
-    //subtraction of two number
+    // subtraction of two numbers
     sub=a-b;
-    printf("subtraction of two numbers is %d\n",sub);
+    printf("subtraction of two numbers is %d\n", sub);
     //division of two numbers
     div=a/b;
-    printf("division of two numbers is %d\n",div);
-    //multiplication of two numbers
+    printf("division of two numbers is %d\n", div);
+    // multiplication of two numbers
     mul=a*b;
-    printf("multiplication of two numbers is %d\n",mul);
-
+    printf("multiplication of two numbers is %d\n", mul);
     return 0;
+
 }
-
-
