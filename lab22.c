93427a2b9c12c5121e1248943db32c7b0f60deef
unknown <baseerfatima01@gmail.com>
2021-04-05 12:48:21 +0530

    program to copy element using string operation

---
+++ lab22.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+ char a[20], b[20];
+
+   printf(" enter the name\n");
+   scanf("%s", a);
+    strcpy(b,a);
+   printf("%s",b );
+   return 0;
+}
