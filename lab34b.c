88223b8f7a8a812e20e7958c82829a3dbed24f85
baseerfatima <75359117+baseerfatima@users.noreply.github.com>
2021-04-06 23:18:37 +0530

    "program to read from a file"

--- lab34b.c
+++ lab34b.c
@@ -6,7 +6,8 @@ int main()
    int num;
    FILE *fptr;
 
-   if ((fptr = fopen("C:\\program.txt","r")) == NULL){
+   if ((fptr = fopen("C:\\program.txt","r")) == NULL)
+   {
        printf("Error! opening file");
 
        exit(1);
